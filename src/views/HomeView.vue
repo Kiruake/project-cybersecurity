5437c72fae7100e8246a756b3a2803e88c160e3a
ayrton chaves <ayrton.chaves@edu.univ-fcomte.fr>
2025-01-21 11:31:23 +0100

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <main>
+
+    <iframe width="640" height="640" src="https://cybermap.kaspersky.com/fr/widget/dynamic/dark" frameborder="0">
+    </iframe>
+
+  </main>
+</template>
